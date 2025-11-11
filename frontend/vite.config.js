05457c037ceed386690dfc31f051cf0e251bcb4a
Gitgo012 <yashkuletha05@gmail.com>
2025-11-11 18:16:33 +0530

    added railway url to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['daring-transformation-production.up.railway.app'],
-    port: 4173, // optional local preview port
+    allowedHosts: ['.railway.app'],
   },
 })
